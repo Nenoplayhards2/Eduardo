9ebd8639a4237a31729f28e50b735b91c33f0599
Nenoplayhard52 <125273548+Nenoplayhards2@users.noreply.github.com>
2023-08-08 17:51:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function mostrarsenha() {
+    var inputPass = document.getElementById('Senha')
+    var btnShowPass = document.getElementById ('btnSenha') 
+
+    if(inputPass.type === 'password'){
+        inputPass.setAttribute('type','text')
+        btnShowPass.classList.replace('bi-eye','bi-eye-slash')
+    }else{
+         inputPass.setAttribute('type','password')
+        btnShowPass.classList.replace('bi-eye-slash','bi-eye')
+    }
+    
+}
